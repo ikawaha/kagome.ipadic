5ff234fe25eff5e2261c2b72ad91b6fd61a3d0ae
ikawaha <ikawaha@users.noreply.github.com>
2016-09-27 23:20:38 +0900

    Backport from kagome

--- tokenizer/dic.go
+++ tokenizer/dic.go
@@ -21,11 +21,6 @@ type Dic struct {
 	dic *dic.Dic
 }
 
-// UserDic represents a user dictionary.
-type UserDic struct {
-	dic *dic.UserDic
-}
-
 // SysDic returns the system dictionary (IPA dictionary).
 func SysDic() Dic {
 	return Dic{dic.SysDic()}
@@ -40,10 +35,4 @@ func SysDicIPA() Dic {
 func NewDic(path string) (Dic, error) {
 	d, err := dic.Load(path)
 	return Dic{d}, err
-}
-
-// NewUserDic build a user dictionary from a file.
-func NewUserDic(path string) (UserDic, error) {
-	d, err := dic.NewUserDic(path)
-	return UserDic{d}, err
 }
